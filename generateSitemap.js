7f24dffb3981666d42d57f6c1a2a4f3d92392fb5
tggSearch <huangdh4@outlook.com>
2023-11-22 14:55:23 +0800

    add-img

--- generateSitemap.js
+++ generateSitemap.js
@@ -3,58 +3,57 @@ const path = require('path');
 
 const markdownFolder = './_posts'; // 指定你的Markdown文件夹路径
 const outputFilePath = './sitemap.xml'; // 指定生成的sitemap.xml文件路径
-const siteUrlPrefix = 'https://muskcoins.github.io/'; // 指定网站URL前缀
+const siteUrlPrefix = 'https://tggsearch.github.io/'; // 指定网站URL前缀
 
 const currentDate = new Date();
-const formattedDate = currentDate.toISOString().split('T')[0];
+const formattedDate = currentDate.toISOString();
 // 开头部分
 let sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-<loc>https://tggsearch.github.io/</loc>
-<lastmod>${formattedDate}</lastmod>
-<changefreq>daily</changefreq>
-<priority>1.0</priority>
-</url>
-<url>
-<loc>https://tggsearch.github.io/telegram.html</loc>
-<lastmod>${formattedDate}</lastmod>
-<changefreq>daily</changefreq>
-<priority>1.0</priority>
-</url>
-<url>
-<loc>https://tggsearch.github.io/twitter.html</loc>
-<lastmod>${formattedDate}</lastmod>
-<changefreq>daily</changefreq>
-<priority>1.0</priority>
-</url>
-<url>
-<loc>https://tggsearch.github.io/account.html</loc>
-<lastmod>${formattedDate}/lastmod>
-<changefreq>daily</changefreq>
-<priority>1.0</priority>
-</url>
-<url>
-<loc>https://tggsearch.github.io/exchange.html</loc>
-<lastmod>2023-06-10T12:25:00+00:00</lastmod>
-<changefreq>daily</changefreq>
-<priority>1.0</priority>
-</url>
+  <url>
+    <loc>https://tggsearch.github.io/</loc>
+    <lastmod>${formattedDate}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://tggsearch.github.io/telegram.html</loc>
+    <lastmod>${formattedDate}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://tggsearch.github.io/twitter.html</loc>
+    <lastmod>${formattedDate}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://tggsearch.github.io/account.html</loc>
+    <lastmod>${formattedDate}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://tggsearch.github.io/exchange.html</loc>
+    <lastmod>${formattedDate}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
 `;
 
 // 读取Markdown文件夹中的文件
 fs.readdirSync(markdownFolder).forEach(file => {
     if (file.endsWith('.md')) {
         const fileNameWithoutExtension = file.replace('.md', '').replace(/^\d{4}-\d{2}-\d{2}-/, ''); // 去掉日期部分
-        const url = `${siteUrlPrefix}${fileNameWithoutExtension}`;
+        const url = `${siteUrlPrefix}docs/${fileNameWithoutExtension}.html`;
 
         const filePath = path.join(markdownFolder, file);
         const stat = fs.statSync(filePath);
-        const lastMod = stat.mtime.toISOString().split('T')[0];
+        const lastMod = stat.mtime.toISOString();
         
         // 生成sitemap条目
-        sitemapContent += `
-  <url>
+        sitemapContent += `<url>
     <loc>${encodeURI(url)}</loc>
     <lastmod>${lastMod}</lastmod>
     <changefreq>monthly</changefreq>
